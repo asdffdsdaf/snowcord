9d7476802ef87d4d8831a45018aa752ae189f6e6
asdffdsdaf <87440869+asdffdsdaf@users.noreply.github.com>
2021-07-16 20:04:08 +0300

    oops

--- src/snowcord.cr
+++ src/snowcord.cr
@@ -7,11 +7,11 @@ module Snowcord
     return self.to_snowflake(Time.utc.to_unix_ms)
   end
 
-  def self.to_snowflake(unixTimestamp)
+  def self.to_snowflake(unixTimestamp : Int)
     return (unixTimestamp - DISCORD_EPOCH) << 22
   end
 
-  def self.to_unix(snowflake)
+  def self.to_unix(snowflake : Int)
     return (snowflake >> 22) + DISCORD_EPOCH
   end
 end
